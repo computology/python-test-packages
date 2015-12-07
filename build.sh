bbd78ab1f05fc2084cbf64565fda03f9406329b4
Armando Canals <armandocanals@gmail.com>
2015-12-07 11:36:58 -0800

    add sdist formats

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ git clean -fXd --exclude .vagrant
 
 python setup.py bdist_wheel --universal
 python setup.py bdist_egg
-python setup.py sdist --formats=gztar,zip
+python setup.py sdist --formats=bztar,zip,gztar,tar,ztar
