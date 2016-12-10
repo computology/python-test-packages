1c8b590d18fb1194d06f111c3c14a9bb6f850a4c
capotej <jcapote@gmail.com>
2016-12-09 22:49:58 -0500

    remove ztar

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ git clean -fXd --exclude .vagrant
 
 python setup.py bdist_wheel --universal
 python setup.py bdist_egg
-python setup.py sdist --formats=bztar,zip,gztar,tar,ztar
+python setup.py sdist --formats=bztar,zip,gztar,tar
